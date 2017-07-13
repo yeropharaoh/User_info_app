9abd5bdbee8756d81e415c9ef530aba105c0daf6
yeropharaoh <yeroschutte@gmail.com>
2017-07-13 17:11:58 +0200

    resubmit

--- src/app.js
+++ src/app.js
@@ -9,119 +9,75 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Part 0
-app.get('/', function (req, res) {
-	fs.readFile('../users.json', function (error, data) {
-		if (error) {
-			console.log(error);
-		}
-		var parsedData = JSON.parse(data);
-		console.log(parsedData);
-		res.render('index', {Users: parsedData});
-	});
+app.get('/', function(req, res) {
+    fs.readFile('../users.json', function(error, data) {
+        if (error) {
+            console.log(error);
+        }
+        var parsedData = JSON.parse(data);
+        // console.log(parsedData);
+        res.render('index', { Users: parsedData });
+    });
 });
 
 //Part 1.A
-app.get('/search', function (req, res) {
+app.get('/search', function(req, res) {
     res.render('pug-searchform');
 });
 
-app.post('/search', function (req, res) {
-		res.render('pug-searchform');
-});
-
 //Part 1.B
-app.post('/match', function(req,res){
-	console.log(req.body.name);
-	
-	fs.readfile('../users.json', 'utf8', function (error, data) {
-		if (error) {
-			throw error;
+app.post('/match', function(req, res) {
+
+    fs.readFile('../users.json', 'utf8', function(error, data) {
+        if (error) {
+            throw error;
+        }
+        var parsedData = JSON.parse(data);
+        console.log(parsedData);
+
+        for (var i = 0; i < parsedData.length; i++) {
+            if(req.body.search === parsedData[i].firstname || req.body.search===parsedData[i].lastname){
+		            
+            var person = parsedData[i];   
+			console.log(parsedData[i]);
 		}
-		var parsedData = JSON.parse(data);
-		console.log(parsedData);
-		
-		for(var i = 0; i < parsedData.length; i++) {
-			if((req.body.name === parsedData[i].firstname) || (req.body.name === parsedData[i].lastname)) { 
-				var result = parsedData[i].firstname + ' ' + parsedData[i].lastname;
-			}
-		}
-		console.log(result);
-		res.render('pug-match', {Users: result});
-	});
+     };
+     res.render('pug-match', {Users: person});
+});
 });
 
 //Part2.A
-app.get('/form', function(req, res){
-	res.render('pug-form')
-});
-	
-app.post('/forminfo', function(req, res){
-	console.log(req.body);
-
-	fs.readFile('../users.json', 'utf8', function(err, data){
-		if (err){
-	 	throw err;
-		} 
-
-	var parsedData = JSON.parse(data); 
-	var newUserData = req.body;
-
-	parsedData.push(newUserData);
-	console.log(parsedData);
-
-	var completeusers = JSON.stringify(parsedData);
-	console.log(completeusers);
-
-//Part2.B
-fs.writeFile('../users.json', 'utf8', function(err, data){
-			if(err){
-				throw err;
-			}
-			res.redirect('/');
-		});
-	});
-
+app.get('/form', function(req, res) {
+    res.render('pug-form')
 });
 
-app.listen(3000, function(){
-	console.log('Listening on port 3000');
+//Part2.B	
+app.post('/forminfo', function(req, res) {
+    console.log(req.body);
+
+    fs.readFile('../users.json', 'utf8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+
+        var parsedData = JSON.parse(data);
+        var newUserData = req.body;
+
+        parsedData.push(newUserData);
+        console.log(parsedData);
+
+        var completeusers = JSON.stringify(parsedData);
+        console.log(completeusers);
+
+        fs.writeFile('../users.json', completeusers, 'utf8', function(err, data) {
+            if (err) {
+                throw err;
+            }
+        });
+    });
+    res.redirect('/');
 });
 
-
-
-
-
-
-
-
-
-
-
-// 	if(requestParameters.firstname!==undefined&&requestParameters.lastname!==undefined){
-// 		res.send('<body><h1> Hallo' + requestParameters.firstname + '' + requestParameters.lastname '</h1><body>')
-// 	}
-// 	else{
-// 		res.send('you did not enter a name');
-// 	}
-// });
-
-// app.post('/match', function(req,res){
-// 	console.log(req.body);
-// 	res.redirect('/match')
-// })
-
-// app.use(bodyParser.json());
-
-
-// app.use(express.static('../public'));
-
-
-
-// app.get('/', function (req, res){
-// 	res.render('index');
-// });
-
-// console.log("post request received");
-	// console.log(request.body);
-	// response.render('/Form');
-	// response.send('data received: ' + JSON.stringify(request.body) + '\n');
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
